775323850ce79c3cc9efff7b1147782d8b557edc
Juri <juri.strumpflohner@r3-gis.com>
2019-04-24 11:06:16 +0200

    test: verify lazy loading works

--- apps/demoapp-e2e/src/integration/app.spec.ts
+++ apps/demoapp-e2e/src/integration/app.spec.ts
@@ -1,9 +1,25 @@
-import { getGreeting } from '../support/app.po';
-
-describe('Hello Nx', () => {
+describe('ngx-lazy-el should lazy load components', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome to demoapp!');
+  it('should load the app', () => {
+    cy.get('juristr-root');
+  });
+
+  it('should lazy load the component', () => {
+    cy.get('[data-cy="lazy-load-button"]').click();
+
+    cy.get('[data-cy="user-list-card"]').as('user-list');
+
+    cy.get('@user-list')
+      .find('mat-list-item')
+      .should('have.length.gt', 1);
+
+    cy.get('@user-list')
+      .find('[data-cy="emit-event-btn"]')
+      .click();
+
+    cy.get('[data-cy="lazy-component-message"]')
+      .find('pre')
+      .should('contain', 'Juri');
   });
 });
